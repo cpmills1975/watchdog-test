328fc8a76ee50941cd28342d7e3efb99e2104006
Chris Mills <1878544+cpmills1975@users.noreply.github.com>
2025-03-18 22:22:37 +0000

    Initial commit

---
+++ watchdog_test.py
@@ -0,0 +1,61 @@
+"""
+Monitor for and record file system changes
+"""
+
+import logging
+import hashlib
+
+from watchdog.events import LoggingEventHandler, FileSystemEvent
+from watchdog.observers import Observer
+
+BUF_SIZE = 65535
+
+
+def hash_file(filename):
+    """
+    Returns a SHA256 hash for a given filename
+    """
+    sha = hashlib.sha256()
+
+    with open(filename, "rb") as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha.update(data)
+        return sha.hexdigest()
+
+
+class MyEventHandler(LoggingEventHandler):
+    """
+    Subclass of LoggingEventHandler adding output of a file hash
+    when a file is modified.
+    """
+
+    def on_modified(self, event: FileSystemEvent) -> None:
+        """
+        Called in response to a file being modified
+        """
+        if event.is_directory:
+            self.logger.info("Modified directory: %s", event.src_path)
+        else:
+            self.logger.info(
+                "Modified file: %s (%s)", event.src_path, hash_file(event.src_path)
+            )
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+
+event_handler = MyEventHandler()
+observer = Observer()
+observer.schedule(event_handler, ".", recursive=True)
+observer.start()
+try:
+    while observer.is_alive():
+        observer.join(1)
+finally:
+    observer.stop()
+    observer.join()
